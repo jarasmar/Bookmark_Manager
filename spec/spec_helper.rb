b9cd1026a5b66ee1be6485257d7441618a261e5d
Student <student@Makerss-MacBook-Air.local>
2020-03-12 12:02:55 +0000

    require file in spec_helper updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,9 @@ ENV['ENVIRONMENT'] = 'test'
 require_relative './setup_test_database'
 
 # require our Sinatra app file
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+# require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require_relative '../app.rb'
+
 
 require 'capybara'
 require 'capybara/rspec'
